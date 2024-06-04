2534e0989fa1f60b9bca3a37e186e2de8dc62226
ydh5017 <ydh5017@naver.com>
2024-06-04 15:45:08 +0900

    init commit

---
+++ src/main/java/com/sparta/javafeed/JavafeedApplication.java
@@ -0,0 +1,14 @@
+package com.sparta.javafeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class JavafeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavafeedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/javafeed/JavafeedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.javafeed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavafeedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
