0824971a97fa02bb42a4bf1c4187a52211a92547
docoding0228 <npng0228@naver.com>
2024-06-05 10:14:42 +0900

    feat: 예외처리, 성공처리 생성
    Exception 관련 파일 생성
    
    resolves: #7

--- src/main/java/com/sparta/javafeed/entity/User.java
+++ src/main/java/com/sparta/javafeed/entity/User.java
@@ -35,7 +35,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
+    @Column
     private String intro;
 
     @Column(nullable = false)
@@ -43,7 +43,7 @@ public class User {
     // Eunm 값이 데이터가 숫자로 저장되기 때문에, 스트링으로 찍히도록 하기위함.
     private UserStatus userStatus;
 
-    @Column(nullable = false)
+    @Column
     private String refreshToken;
 
     @CreatedDate
